c0db645d9e608c31fc96759a440758b3ed34ddba
Daria Kotowicz <dasha.kotovich@gmail.com>
2016-12-01 16:23:02 +0100

    no message

---
+++ projekt_1/src/main/java/com/model/Nieruchomosc.java
@@ -0,0 +1,57 @@
+package main.java.com.model;
+
+public class Nieruchomosc 
+{
+	private int id_nieruchomosc;
+	private String miasto;
+	private String kod_pocztowy;
+	private float czynsz;
+	private String ulica;
+	private String nr_bloku;
+	private int id_posrednik;
+
+	public int get_id_nieruchomosc() {
+		return id_nieruchomosc;
+	}
+	public void set_id_nieruchomosc(int id_nieruchomosc) {
+		this.id_nieruchomosc = id_nieruchomosc;
+	}
+	public String get_miasto() {
+		return miasto;
+	}
+	public void set_miasto(String miasto) {
+		this.miasto = miasto;
+	}
+	public String get_kod_pocztowy() {
+		return kod_pocztowy;
+	}
+	public void set_kod_pocztowy(String kod_pocztowy) {
+		this.kod_pocztowy = kod_pocztowy;
+	}
+
+	public float get_czynsz() {
+		return czynsz;
+	}
+	public void set_czynsz(float czynsz) {
+		this.czynsz = czynsz;
+	}
+	public String get_ulica() {
+		return ulica;
+	}
+	public void set_ulica(String ulica) {
+		this.ulica = ulica;
+	}
+	public String get_nr_bloku() {
+		return nr_bloku;
+	}
+	public void set_nr_bloku(String nr_bloku) {
+		this.nr_bloku = nr_bloku;
+	}
+	public int get_id_posrednik() {
+		return id_posrednik;
+	}
+	public void set_id_posrednik(int id_posrednik) {
+		this.id_posrednik = id_posrednik;
+	}
+
+}
